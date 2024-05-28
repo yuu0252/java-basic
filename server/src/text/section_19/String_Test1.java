fcc433317311168fecfb2e203e32b7611e396724
yuu0252 <yutanmaron@gmail.com>
2024-05-28 22:31:18 +0900

    create String

---
+++ server/src/text/section_19/String_Test1.java
@@ -0,0 +1,12 @@
+package text.section_19;
+
+public class String_Test1 {
+    public static void main(String[] args) {
+        String moji1 = "テスト";
+        String moji2 = "テスト";
+        String moji3 = "サンプル";
+
+        System.out.println(moji1.equals(moji2));
+        System.out.println(moji1.equals(moji3));
+    }
+}
